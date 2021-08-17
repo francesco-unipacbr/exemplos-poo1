2267f423891e1e4f9c5f2d6b7d1c5fdcb407fcef
Francesco Silva <francesco.silva@vallourec.com>
2021-08-16 22:19:17 -0300

    Primeiro commit: subindo o projeto de exemplos

---
+++ Exemplos/src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+/**
+ *
+ * @author franc
+ */
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello world!");
+    }    
+}
