ffa102323f5aeeb880f12a983de2de60a8cc70ad
Francesco Silva <francesco.silva@vallourec.com>
2021-08-16 22:23:20 -0300

    Inserindo nova mensagem

--- Exemplos/src/Main.java
+++ Exemplos/src/Main.java
@@ -6,5 +6,6 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JOptionPane.showMessageDialog(null, "Hello world!");
+        JOptionPane.showMessageDialog(null, "Nova mensagem!!");
     }    
 }
