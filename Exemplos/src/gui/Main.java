76cef4887f47043ed97d43e29d83fc4ebc6e8333
Francesco Silva <francesco.silva@unipac.br>
2021-09-27 23:01:48 -0300

    Introdução à GUI

---
+++ Exemplos/src/gui/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author franc
+ */
+public class Main {
+    public static void main(String args[]) {
+        Login telaLogin = new Login();
+        telaLogin.setVisible(true);
+    }
+}
